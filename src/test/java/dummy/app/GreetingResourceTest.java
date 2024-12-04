da7b84fa73aabccdf21ee0cae5d70eff4e2aac68
lemorse <arthurr.caron@gmail.com>
2024-12-04 11:13:12 +0100

    Hello jauuuuuune

--- src/test/java/dummy/app/GreetingResourceTest.java
+++ src/test/java/dummy/app/GreetingResourceTest.java
@@ -15,7 +15,7 @@ public class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("hello jaune"));
+             .body(is("hello jauuuuuune"));
     }
 
 }
